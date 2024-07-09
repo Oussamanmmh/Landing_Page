3a11c31e2c0c1e3c0f380c4228c677deff841d13
oussamanmh <mo_nemamcha@esi.dz>
2024-07-09 17:25:56 +0200

    last commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,20 @@
-
+import logo1 from '../assets/chercher.png'
+import logo2 from '../assets/facebook.png'
+import logo3 from '../assets/twitter.png'
+import logo4 from '../assets/linkedin.png'
 export const FooterPage=()=>{
     return(
         <>
-        <footer className='flex justify-between t-4 p-5 '>
+        <footer className='flex justify-center gap-20  t-4 p-10 '>
             <div className="text-gray-400 text-sm">
                 More than 14,0000 businesses trust Stretch to help them grow.
             </div>
-            <div className='flex'>
+            <div className='flex gap-20'>
+                <img src={logo1} alt = "logo" className="w-10 h-10"/>
+                <img src={logo2} alt = "logo" className="w-10 h-10"/>
+                <img src={logo3} alt = "logo" className="w-10 h-10"/>
+                <img src={logo4} alt = "logo" className="w-10 h-10"/>
+
                
             </div>
         </footer>
--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -5,22 +5,22 @@ export const NavBar=() =>{
             <a className="text-5xl font-bold text-black" href="/">Stretch.</a>
             <div className="">
                 <ul className="flex flex-row justify-between gap-10 items-center  text-sm" >
-                    <li ><a href="/#">Platfom</a>
+                    <li  className="hover:bg-gray-400 rounded-xl py-2 hover:cursor-pointer px-3"><a href="/#" className="">Platfom</a>
                     <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" ml-2 h-4 w-4 inline-block">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                     </svg>
 </li>
-                    <li ><a href="/#">Use cases</a> <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" ml-2 h-4 w-4 inline-block">
+                    <li className="hover:bg-gray-400 rounded-xl py-2 hover:cursor-pointer px-3"><a href="/#">Use cases</a> <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" ml-2 h-4 w-4 inline-block">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                     </svg></li>
-                    <li ><a href="/#">Why Stretch</a></li>
-                    <li ><a href="/#">Resources</a></li>
-                    <li ><a href="/#">Pricing</a></li>
+                    <li className="hover:bg-gray-400 rounded-xl py-2 hover:cursor-pointer px-3"><a href="/#">Why Stretch</a></li>
+                    <li className="hover:bg-gray-400 rounded-xl py-2 hover:cursor-pointer px-3" ><a href="/#">Resources</a></li>
+                    <li  className="hover:bg-gray-400 rounded-xl py-2 hover:cursor-pointer px-3"><a href="/#">Pricing</a></li>
                 </ul>
             </div>
             <div className="flex justify-center items-center gap-8">
                 <a href="/Login" className="">Login</a>
-                <a className="border-2 border-black p-2 rounded-lg  " href="/sign up">Get started</a>
+                <a className="border-2 border-black p-2 transition ease-in-out duration-100 rounded-lg hover:bg-orange-600 hover:text-white hover:border-orange-600  " href="/sign up">Get started</a>
             </div>
            
         </div>
